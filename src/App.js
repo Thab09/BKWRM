4921765d96248da46b3089b679eb581a3c9a22f2
Ibraaheem bin Ahmed <ibraheem.thab@gmail.com>
2022-07-20 22:24:52 +0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/app.scss";
 
 import Home from "./pages/Home";
 import Library from "./pages/Library";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
+import Reading from "./pages/Bookshelf/Reading";
+import PlanToRead from "./pages/Bookshelf/PlanToRead";
+import Dropped from "./pages/Bookshelf/Dropped";
+import Completed from "./pages/Bookshelf/Completed";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/bookshelf/reading" element={<Reading />} />
+            <Route path="/bookshelf/plantoread" element={<PlanToRead />} />
+            <Route path="/bookshelf/dropped" element={<Dropped />} />
+            <Route path="/bookshelf/completed" element={<Completed />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,38 +10,50 @@ function Navbar() {
       </Link>
       <NavLink
         to="/library"
-        className={({ isActive }) => (isActive ? "link active" : "")}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         <h4>Library</h4>
       </NavLink>
-      <Link to="/">
+      <Link to="/bookshelf/reading">
         <h4>Bookshelf</h4>
       </Link>
 
       <div className="bookshelf">
-        <NavLink to="/">
+        <NavLink
+          to="/bookshelf/reading"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
           <h4>Reading</h4>
         </NavLink>
-        <NavLink to="/">
+        <NavLink
+          to="/bookshelf/plantoread"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
           <h4>Plan to Read</h4>
         </NavLink>
-        <NavLink to="/">
+        <NavLink
+          to="/bookshelf/dropped"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
           <h4>Dropped</h4>
         </NavLink>
-        <NavLink to="/">
+        <NavLink
+          to="/bookshelf/completed"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
           <h4>Completed</h4>
         </NavLink>
       </div>
 
       <NavLink
         to="/search"
-        className={({ isActive }) => (isActive ? "active" : "")}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         <h4>Search</h4>
       </NavLink>
       <NavLink
         to="/profile"
-        className={({ isActive }) => (isActive ? "active" : "")}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
       >
         <h4>Profile</h4>
       </NavLink>
