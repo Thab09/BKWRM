3404e8d534296170d8b2cafdb952814d6bf7022f
Ibraaheem bin Ahmed <ibraheem.thab@gmail.com>
2022-07-25 00:20:47 +0500

    added search by to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,33 +6,49 @@ function SearchBar() {
   const [input, setInput] = useState("");
   //   const navigate = useNavigate();
 
+  const [searchBy, setSearchBy] = useState("book");
+
   const submitHandler = (e) => {
     e.preventDefault();
+    console.log(input, searchBy);
     setInput("");
   };
 
   return (
-    <div className="search-container">
-      <form onSubmit={submitHandler}>
-        <div className="search">
-          <input
-            className="searchbar"
-            type="text"
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
-          />
-          <input type="submit" value="search" className="searchbutton" />
-        </div>
-        <div className="searchby">
-          <span>search by</span>
-          <ul>
-            <li>book</li>
-            <li>author</li>
-            <li>text</li>
-          </ul>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={submitHandler}>
+      <div className="search">
+        <input
+          className="searchbar"
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <input type="submit" value="search" className="searchbutton" />
+      </div>
+      <div className="searchby">
+        <span>search by:</span>
+        <ul>
+          <li
+            onClick={(e) => setSearchBy(e.target.textContent)}
+            className={searchBy === "book" ? "active" : ""}
+          >
+            book
+          </li>
+          <li
+            onClick={(e) => setSearchBy(e.target.textContent)}
+            className={searchBy === "author" ? "active" : ""}
+          >
+            author
+          </li>
+          <li
+            onClick={(e) => setSearchBy(e.target.textContent)}
+            className={searchBy === "text" ? "active" : ""}
+          >
+            text
+          </li>
+        </ul>
+      </div>
+    </form>
   );
 }
 
